d9861bc07c631fc48609cd0b96e6bb95a5bc6c0f
Almaz Gainutdinov <333almaz@gmail.com>
2023-08-11 21:15:35 +0300

    Added one script from course

---
+++ script_2.2.8.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "https://SunInJuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    
+    
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    
+    first_input = browser.find_element(By.CSS_SELECTOR, ".form-control")
+    first_input.send_keys(y)
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "[value='robots']")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", option2)
+    option2.click()
+
+
+    # # Проверяем, что смогли зарегистрироваться
+    # # ждем загрузки страницы
+    # time.sleep(1)
+
+    # # находим элемент, содержащий текст
+    # welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    # welcome_text = welcome_text_elt.text
+
+    # # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    # assert "Congratulations! You have successfully registered!" == welcome_text
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    
+
+# не забываем оставить пустую строку в конце файла
